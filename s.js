d3d9db6f79e824d7c67deb9901a1a0369d9d06a9
程中原 <1651333184@qq.com>
2026-05-15 15:24:02 +0800

    remove temp file

--- s.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('ssh2');
-const c = new Client();
-c.on('ready', () => {
-  c.exec('cd ~/my-blog && git pull && pm2 reload my-blog', (e, s) => {
-    s.stderr.on('data', d => process.stderr.write(d));
-    s.on('close', () => { console.log('ok'); c.end(); });
-  });
-});
-c.connect({ host: '49.232.244.54', port: 22, username: 'zhongyuan', password: '050129czy', readyTimeout: 10000 });
